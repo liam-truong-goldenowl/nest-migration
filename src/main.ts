5b9aa550db0ef71d051a3a6eef3f027bb106a1b1
Liam Truong <liam.truong.goldenowl@gmail.com>
2025-07-09 16:55:12 +0700

    style: split code sections with blank lines

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+
   const port = configService.get<number>('PORT') ?? 3000;
   await app.listen(port);
 }
+
 void bootstrap();
